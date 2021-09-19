b35f1f384be4f15c336ae21c7b2d04a83f0d7335
Anton Bauer <abton.bauer@gmail.com>
2021-09-19 18:39:13 +0200

    Add top-level logging

--- TargetEvaluation.Cli/TargetEvaluationService.cs
+++ TargetEvaluation.Cli/TargetEvaluationService.cs
@@ -1,19 +1,34 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace TargetEvaluation.Cli
 {
     internal class TargetEvaluationService : IHostedService
     {
+        private readonly ILogger _logger;
+        private readonly TargetEvaluationOptions _options;
+
+        public TargetEvaluationService(
+            ILogger<TargetEvaluationService> logger,
+            IOptions<TargetEvaluationOptions> options)
+        {
+            _logger = logger;
+            _options = options.Value;
+        }
+
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation("Started evaluation");
+            return ValueTask.CompletedTask.AsTask();
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation("Stopped");
+            return ValueTask.CompletedTask.AsTask();
         }
     }
 }
